d664da8d0d2d9b13f488b3adb2b8316f1079f662
Catherine Gao-Howard <63123705+cloverbunny@users.noreply.github.com>
2023-12-11 16:02:25 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st 
+import openai
+
+from openai import OpenAI 
+
+st.write('Hello world')
+
+def calculate_bmi(weight, height):
+    bmi = (weight/height**2)*703
+    return bmi 
+
+weight = st.number_input('Insert weight (lbs)',value=90,)
+st.write('The current number in lbs is ', weight)
+
+height = st.number_input('Insert height (inches)',value=60)
+st.write('The height in inches is ', height)
+
+if st.button('Click here to calculate BMI'):
+    bmi = calculate_bmi(weight,height)
+    st.write('The BMI is ', bmi)
+    if bmi<18.5:
+        st.write('You are underweight')
+    elif bmi>18.5 & bmi<25:
+        st.write('You are a normal weight')
+
+
