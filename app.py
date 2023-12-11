d7ac041a2a664daee3000d77de705459c7cf1b92
Catherine Gao-Howard <63123705+cloverbunny@users.noreply.github.com>
2023-12-11 16:46:31 -0600

    fix obese

--- app.py
+++ app.py
@@ -9,20 +9,22 @@ def calculate_bmi(weight, height):
     return bmi 
 
 weight = st.number_input('Insert weight (lbs)',value=90,)
-st.write('The current number in lbs is ', weight)
+st.write('Your weight in lbs is ', weight)
 
 height = st.number_input('Insert height (inches)',value=60)
-st.write('The height in inches is ', height)
+st.write('Your height in inches is ', height)
 
 if st.button('Click here to calculate BMI'):
     bmi = calculate_bmi(weight,height)
-    st.write('The BMI is ', bmi)
+    st.write('Your BMI is ', bmi)
     if bmi<18.5:
-        st.write('You are underweight')
+        st.write('You are underweight.')
     elif (bmi>18.5) and (bmi<25):
-        st.write('You are a normal weight')
+        st.write('You are a normal weight.')
+    elif bmi>25 and bmi<30:
+        st.write('You are overweight.')
     else:
-        st.write('You are overweight')
+        st.write('You are obese.')
 
 
 st.write('''
@@ -30,6 +32,7 @@ st.write('''
          If your BMI is less than 18.5, it falls within the underweight range. \n  
          If your BMI is 18.5 to <25, it falls within the healthy weight range. \n  
          If your BMI is 25.0 to <30, it falls within the overweight range. \n  
+         If your BMI is 30.0 or higher, it falls within the obesity range.
          ''')
 
 
